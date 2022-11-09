32c8e7627a3dd38f49e72f7f9899223f85dfda87
whchoi78 <caliper78@naver.com>
2022-11-09 13:48:45 +0900

    "1.2"

--- upgrade_ssh_ssl.sh
+++ upgrade_ssh_ssl.sh
@@ -79,8 +79,8 @@ echo '' && echo '##Completed install Openssh'
 cp ./contrib/sshd.pam.generic /etc/pam.d/sshd
 cp -p contrib/redhat/sshd.init /etc/init.d/sshd
 chmod +x /etc/init.d/sshd
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 echo "Port 202" >> /etc/ssh/sshd_config
+echo "Port 10022" >> /etc/ssh/sshd_config
 systemctl enable sshd
 systemctl restart sshd
 
